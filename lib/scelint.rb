ddd10bb6e449e60f40fd31140bd274b548a4cfe2
Steven Pritchard <steve@sicura.us>
2022-01-28 10:12:16 -0600

    (#9) Clean up for rubocop
    - Fixes #9

--- lib/scelint.rb
+++ lib/scelint.rb
@@ -46,7 +46,7 @@ module Scelint
         lint(file, data)
       end
 
-      @data
+      @data # rubocop:disable Lint/Void
     end
 
     def parse(file)
@@ -70,13 +70,9 @@ module Scelint
       @data.keys - ['merged data']
     end
 
-    def warnings
-      @warnings
-    end
+    attr_reader :warnings
 
-    def errors
-      @errors
-    end
+    attr_reader :errors
 
     def check_version(file, data)
       @errors << "#{file}: version check failed" unless data['version'] == '2.0.0'
@@ -91,7 +87,7 @@ module Scelint
         'controls',
       ]
 
-      data.keys.each do |key|
+      data.each_key do |key|
         @warnings << "#{file}: unexpected key '#{key}'" unless ok.include?(key)
       end
     end
@@ -185,7 +181,7 @@ module Scelint
       ]
 
       data.each do |profile, value|
-        value.keys.each do |key|
+        value.each_key do |key|
           @warnings << "#{file} (profile '#{profile}'): unexpected key '#{key}'" unless ok.include?(key)
         end
 
@@ -211,7 +207,7 @@ module Scelint
       ]
 
       data.each do |ce, value|
-        value.keys.each do |key|
+        value.each_key do |key|
           @warnings << "#{file} (CE '#{ce}'): unexpected key '#{key}'" unless ok.include?(key)
         end
 
@@ -234,19 +230,18 @@ module Scelint
     end
 
     def check_remediation(file, check, remediation_section)
-      
       reason_ok = [
-        'reason'
+        'reason',
       ]
 
       risk_ok = [
         'level',
-        'reason'
+        'reason',
       ]
 
       if remediation_section.is_a?(Hash)
         remediation_section.each do |section, value|
-          #require 'pry-byebug'; binding.pry if section == 'disabled'
+          # require 'pry-byebug'; binding.pry if section == 'disabled'
           case section
           when 'scan-false-positive', 'disabled'
             value.each do |reason|
@@ -285,7 +280,7 @@ module Scelint
       end
     end
 
-    def check_value(file, check, value)
+    def check_value(_file, _check, _value)
       # value could be anything
       true
     end
@@ -310,7 +305,7 @@ module Scelint
         @errors << "#{file} (check '#{check}'): missing parameter"
       end
 
-      data.keys.each do |key|
+      data.each_key do |key|
         @warnings << "#{file} (check '#{check}'): unexpected key '#{key}'" unless ok.include?(key)
       end
     end
@@ -332,7 +327,7 @@ module Scelint
         'oval-ids',
         'ces',
         'confine',
-        'remediation'
+        'remediation',
       ]
 
       data.each do |check, value|
@@ -342,7 +337,7 @@ module Scelint
         end
 
         if value.is_a?(Hash)
-          value.keys.each do |key|
+          value.each_key do |key|
             @warnings << "#{file} (check '#{check}'): unexpected key '#{key}'" unless ok.include?(key)
           end
         else
@@ -351,7 +346,9 @@ module Scelint
 
         check_type(file, check, value['type']) if value['type'] || file == 'merged data'
         check_settings(file, check, value['settings']) if value['settings'] || file == 'merged data'
-        check_remediation(file, check, value['remediation']) if value['remediation'] unless value['remediation'].nil?
+        unless value['remediation'].nil?
+          check_remediation(file, check, value['remediation']) if value['remediation']
+        end
         check_controls(file, value['controls']) unless value['controls'].nil?
         check_identifiers(file, value['identifiers']) unless value['identifiers'].nil?
         check_oval_ids(file, value['oval-ids']) unless value['oval-ids'].nil?
